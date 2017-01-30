02d24e8fbf8306e9bb9343e12dd9c97aa4234211
Tom  Christian <tctc91@gmail.com>
2017-01-30 23:21:16 +0000

    write to file

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var express = require('express');
-var fs = require('fs');
-var app     = express();
-var osmosis = require('osmosis');
+const express = require('express');
+const fs = require('fs');
+const app     = express();
+const osmosis = require('osmosis');
+
+const creditJson = { marketplaceCredit : '', ipsthemesCredit : ''};
+const ipsCredentials = {
+  auth: 'tctc91@gmail.com',
+  password: 'nfoISoxAWn6ZIUz'
+}
 
 app.get('/scrape', function(req, res){
   scrapeIpsCredit();
@@ -9,18 +15,18 @@ app.get('/scrape', function(req, res){
 
 function scrapeIpsCredit() {
   osmosis
-  .get('https://invisionpower.com/login/')
-  .submit('form.ipsForm', {
-    'auth': 'tctc91@gmail.com',
-    'password': 'nfoISoxAWn6ZIUz'
-  })
-  //.follow('.cClientAreaLink > a')
-  .get('https://invisionpower.com/clients/credit/')
-  .find('.cNexusCredit_total')
-  .set('title')
-  .data(console.log)
+    .get('https://invisionpower.com/login/')
+    .submit('form.ipsForm', ipsCredentials)
+    .get('https://invisionpower.com/clients/credit/')
+    .find('.cNexusCredit_total')
+    .set('accountCredit')
+    .data((obj) => creditJson.marketplaceCredit = obj.accountCredit)
+    .done(() => writeToFile())
 }
 
+function writeToFile() {
+  fs.writeFile('mp-credit.json', JSON.stringify(creditJson, null, 4));
+}
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
