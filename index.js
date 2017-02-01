9feb38747f38a944a55d646b9d1b12761afeb12e
Tom Christian <tom.christian@neoworks.com>
2017-02-01 17:14:31 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const app     = express();
 const osmosis = require('osmosis');
 
-const creditJson = { marketplace: null, ipsthemes : null };
+const creditJson = { marketplace: null };
 const ipsData = {
   loginPage: 'https://invisionpower.com/login/',
   creditPage: 'https://invisionpower.com/clients/credit/',
@@ -35,8 +35,8 @@ function setCredit(credit) {
 }
 
 function writeToFile() {
-  fs.writeFile('mp-credit.json', JSON.stringify(creditJson, null, 4));
+  fs.writeFile('mp-credit.json', JSON.stringify(creditJson));
 }
 
 app.listen('8081')
-exports = module.exports = app;
+exports = module.exports = scrapeIpsCredit();
